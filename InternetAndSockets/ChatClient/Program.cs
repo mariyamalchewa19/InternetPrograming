55bf2d62f4f855cdd6217eec4bc435bc379b3d7c
Mariya <mariqmalcheva@abv.bg>
2019-11-30 12:59:27 +0200

    Added new Solution & apps ChatClient & ChatServer

--- InternetAndSockets/ChatClient/Program.cs
+++ InternetAndSockets/ChatClient/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
-
-namespace ChatClient
+﻿namespace ChatClient
 {
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port: 11000);
+
+            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(remoteEP);
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                while (true)
+                {
+                    Console.Write(">");
+                    string message = Console.ReadLine();
+                    byte[] msg = Encoding.ASCII.GetBytes(s: message + "<EOF>");
+
+                    int bytesSent = sender.Send(msg);
+
+                    if (message == "exit")
+                        break;
+
+                }
+                sender.Shutdown(how: SocketShutdown.Both);
+                sender.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
+
--- InternetAndSockets/ChatServer/Program.cs
+++ InternetAndSockets/ChatServer/Program.cs
@@ -1,12 +1,67 @@
-﻿using System;
-
-namespace ChatServer
+﻿namespace ChatServer
 {
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;      
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(hostName);
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port: 11000);
+
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                byte[] buffer = new byte[1024];
+
+                listener.Bind(localEndPoint);
+                listener.Listen(backlog: 100);
+
+                Socket handle = listener.Accept();
+
+                while (true)
+                {
+                    string message = "";
+
+                    while (true)
+                    {
+                        int messageSize = handle.Receive(buffer);
+                        message += Encoding.ASCII.GetString(buffer, index: 0, count: messageSize);
+
+                        if (message.Contains("<EOF>"))
+                        {
+                            message = message.Replace(oldValue: "<EOF>", newValue: "");
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("> " + message);
+
+                    if (message == "exit")
+                    {
+                        handle.Shutdown(how: SocketShutdown.Both);
+
+                        handle.Close();
+                        break;
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Goodbye");
+            Console.ReadKey(intercept: true);
         }
     }
 }
+
